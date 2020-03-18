069254cb970e3002243c28ef38b0b7ec77576dca
akshatd4 <akshatd4@gmail.com>
2020-03-19 01:48:21 +0530

    Fixed port

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ var app = express();
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+app.listen(process.env.PORT || "8080", () => {
+  console.log("Running on port ", process.env.PORT || "8080");
+});
